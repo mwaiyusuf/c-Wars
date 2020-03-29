845c915c54e822c46086b2eb4534efbd90472151
Mwaiyusuf <mwaigalo5@gmail.com>
2020-03-29 14:20:52 +0300

    string increament in python

---
+++ 5kyu/increament_string/index.py
@@ -0,0 +1,10 @@
+def increment_string(strng):
+    new_string = strng.split();
+    for i in new_string:
+          if(new_string[-1].isnan):
+               return new_string + '1';
+          else:
+            return new_string[-1]+1
+             
+#         if(new_string.len - 1.isnan)
+#     return new_strng
