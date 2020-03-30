8deabbc9cbae8d513d575ab63a65f572f096de6e
Mwaiyusuf <mwaigalo5@gmail.com>
2020-03-30 19:42:00 +0300

    ranking position using js

---
+++ 7ky/ranking_position/index.js
@@ -0,0 +1,20 @@
+function rankingPosition(){
+    
+    var sortedPeople ={};
+
+    var people = [
+        {
+            'name': 'fahim',
+            'points': 450
+        },
+        {
+            'name': 'fahad',
+            'points': 500
+        },
+        {
+            'name':'farid',
+            'points': 300
+        },
+    ]
+
+}
