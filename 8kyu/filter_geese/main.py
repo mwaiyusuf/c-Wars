0464e51fc6aebb119a8d9fd3d46014e192df2a71
Mwaiyusuf <mwaigalo5@gmail.com>
2020-04-01 22:08:53 +0300

    main.py

---
+++ 8kyu/filter_geese/main.py
@@ -0,0 +1,14 @@
+geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+def goose_filter(birds):
+    #your code here
+    if(birds in geese):
+        return True
+    else:
+        return False
+
+filtered_geese = filter(geese, birds)
+
+print('The filtered geese are:')
+for geese in filtered_geese:
+    print(geese)
