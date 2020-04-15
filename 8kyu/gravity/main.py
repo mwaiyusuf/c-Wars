73fbe16a96caae1c5924df636dc398a39567026e
Mwaiyusuf <mwaigalo5@gmail.com>
2020-04-15 21:41:40 +0300

    python code

---
+++ 8kyu/gravity/main.py
@@ -0,0 +1,9 @@
+def solution(arr_val, arr_unit) :
+    # your code goes here
+       
+       G = 6.67e-11 ;
+       conversion = {
+          'kg':'1', 'g':'1e-3', 'mg':'1e-6', 'μg':'1e-9', 'lb':'.453592'
+          , 'm':'1', 'cm':'1e-2', 'mm':'1e-3', 'μm':'1e-6', 'ft':'.3048'
+       };
+       return G * m1 * conversion[um1] * m2 * conversion[um2] / ( d * conversion[ud] ) ** 2 ;
