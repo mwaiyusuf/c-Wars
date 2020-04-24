44f910e2b5c2a7e621feab7d36db12fc6f5050eb
Mwaiyusuf <mwaigalo5@gmail.com>
2020-04-24 21:56:49 +0300

    indentation error

--- 8kyu/is_opp/main.py
+++ 8kyu/is_opp/main.py
@@ -3,9 +3,3 @@ def is_opposite(s1,s2):
       return False;
    else:
       return True;
-def is_opposite(s1,s2):
-    # your code here
-    if(s1!=s2):
-       return False;
-    else:
-       return True;
